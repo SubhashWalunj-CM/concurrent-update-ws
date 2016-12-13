a21cae1a0926f57f966f11c4a07c79af47441a14
Subahsh Walunj <SWalunj@edrnet.com>
2016-12-13 22:49:26 +0530

    Updating changed values for each form field

--- public/app.js
+++ public/app.js
@@ -1,13 +1,10 @@
 (function () {
-    var concurrentUpdate = angular.module('concurrentUpdate', []);
+    var concurrentUpdate = angular.module('concurrentUpdate', ['ui.bootstrap']);
 
     concurrentUpdate.value('webSocketConstants', {
         webSocketUrl: 'ws://127.0.0.1:1337',
         concurrentUpdateWsCon: {}
     });
-    /*concurrentUpdate.run(function ($rootScope) {
-        $rootScope["concurrentUpdateWsCon"] = new WebSocket('ws://127.0.0.1:1337');
-    });*/
 
     var EditPatientController = function ($scope) {
         $scope.isProviderSelected = false;
@@ -42,6 +39,11 @@
                 providerId: 1001,
                 userId: 100103,
                 name: "Subhash Walunj"
+            },
+            {
+                providerId: 1001,
+                userId: 100104,
+                name: "Will Smith"
             },
             {
                 providerId: 1002,
@@ -133,23 +135,40 @@
     concurrentUpdate.controller('EditPatientController', EditPatientController);
 
     var syncConcurrentUpdateDirective = function ($rootScope, $sce, webSocketConstants) {
+        function formatArrayJoin(arr, separator, lastSeparator) {
+            var outStr = "";
+            if (arr.length === 1) {
+                outStr = arr[0];
+            } else if (arr.length === 2) {
+                outStr = arr.join(' ' + lastSeparator + ' ');
+            } else if (arr.length > 2) {
+                outStr = arr.slice(0, -1).join(separator + ' ') + ' ' + lastSeparator + ' ' + arr.slice(-1);
+            }
+            return outStr;
+        }
+
         return {
             require: "form",
             link: function (scope, element, attrs) {
+                scope.dynamicPopover = {};
+                scope.dynamicPopover[attrs.name] = {};
                 scope.$watch('enableControls', function (newValue, oldValue) {
                     if (newValue) {
-                        var formFieldData = {
-                            "firstName": "John",
-                            "lastName": "Doe",
-                            "contactNo": "",
-                            "gender": "M"
-                        };
+                        var formFieldData = {};
+                        var dirtyFormControls = [];
+                        var form = scope[attrs.name];
+                        angular.forEach(form, function (value, key) {
+                            if (typeof value === 'object' && value.hasOwnProperty('$modelValue')) {
+                                formFieldData[value.$name] = value.$modelValue;
+                                dirtyFormControls.push(value);
+                            }
+                        });
+
                         var reqJson = {
                             "providerId": scope.selectedProvider,
                             "patientId": scope.selectedPatient,
                             "formId": attrs.id,
-                            "userId": scope.selectedUser,
-                            "fields": formFieldData
+                            "userId": scope.selectedUser
                         };
                         scope.alertClass = "alert-info";
 
@@ -158,15 +177,23 @@
                         }
 
                         window.WebSocket = window.WebSocket || window.MozWebSocket;
-                        // if browser doesn't support WebSocket, just show some notification and exit
                         if (!window.WebSocket) {
                             scope.alertClass = "alert-warning";
                             scope.alertMsg = $sce.trustAsHtml("Sorry, but your browser doesn support WebSockets. We cannot assist you in concurrent form updation.");
                         } else {
+                            function triggerFormChangeReq(form, formFieldData, reqJson) {
+                                angular.forEach(form, function (value, key) {
+                                    if (typeof value === 'object' && value.hasOwnProperty('$modelValue')) {
+                                        formFieldData[value.$name] = value.$modelValue;
+                                    }
+                                });
+                                reqJson["type"] = "FORM-CHANGE-REQ";
+                                reqJson["fields"] = formFieldData;
+                                webSocketConstants.concurrentUpdateWsCon.send(JSON.stringify(reqJson));
+                            }
+
                             webSocketConstants.concurrentUpdateWsCon.onopen = function () {
-                                scope.alertClass = "alert-success";
-                                scope.alertMsg = $sce.trustAsHtml("Ready to roll with WebSocket!");
-                                scope.$apply();
+                                reqJson["type"] = "REGISTER-CONNECTION-REQ";
                                 webSocketConstants.concurrentUpdateWsCon.send(JSON.stringify(reqJson));
                             };
 
@@ -176,8 +203,88 @@
                                 scope.$apply();
                             };
 
-                            element.on("change", function () {
-                                webSocketConstants.concurrentUpdateWsCon.send(JSON.stringify(reqJson));
+                            webSocketConstants.concurrentUpdateWsCon.onmessage = function (message) {
+                                if ('data' in message) {
+                                    var resp = JSON.parse(message.data);
+
+                                    switch (resp.type) {
+                                    case "WORKING-USER-RESP":
+                                        var workingUserNameArr = [];
+                                        if ("workingUsers" in resp && Array.isArray(resp.workingUsers)) {
+                                            var index = resp.workingUsers.indexOf(scope.selectedUser);
+                                            if (index > -1) {
+                                                resp.workingUsers.splice(index, 1);
+                                            }
+
+                                            angular.forEach(scope.users, function (value, key) {
+                                                for (var i = 0; i < resp.workingUsers.length; i++) {
+                                                    if (resp.workingUsers[i] == value.userId) {
+                                                        workingUserNameArr.push(value.name);
+                                                    }
+                                                }
+                                            });
+
+                                            if (workingUserNameArr.length) {
+                                                scope.alertClass = "alert-info";
+                                                scope.alertMsg = $sce.trustAsHtml("<strong>" + formatArrayJoin(angular.copy(workingUserNameArr), ',', 'and') + "</strong> is working on this form.");
+                                                scope.$apply();
+
+                                                triggerFormChangeReq(form, formFieldData, reqJson);
+                                            }
+                                        }
+                                        break;
+                                    case "FORM-CHANGE-RESP":
+                                        if (scope.selectedUser in resp) {
+                                            delete resp[scope.selectedUser];
+                                        }
+
+                                        //if (typeof userFormData === 'object' && "fields" in userFormData) {
+                                        angular.forEach(dirtyFormControls, function (control, controlKey) {
+                                            var str = "";
+                                            //scope.dynamicPopover[attrs.name][control.$name] = "";
+                                            angular.forEach(resp, function (userFormData, userFormKey) {
+                                                angular.forEach(userFormData.fields, function (userFormFieldValue, userFormFieldKey) {
+                                                    //scope.dynamicPopover[attrs.name][userFormFieldKey] = "";
+                                                    if (control.$name == userFormFieldKey) {
+                                                        var userName = "";
+                                                        angular.forEach(scope.users, function (value, key) {
+                                                            if (userFormKey == value.userId) {
+                                                                userName = value.name;
+                                                            }
+                                                        });
+                                                        str += "<strong>" + userName + ":</strong> " + userFormFieldValue + "<br>";
+                                                    }
+                                                });
+                                            });
+                                            scope.dynamicPopover[attrs.name][control.$name] = $sce.trustAsHtml(str);
+                                            scope.$apply();
+                                        });
+                                        /*angular.forEach(userFormData.fields, function (userFormFieldValue, userFormFieldKey) {
+                                            //scope.dynamicPopover[attrs.name][userFormFieldKey] = "";
+                                            angular.forEach(dirtyFormControls, function (value, key) {
+                                                if (value.$name == userFormFieldKey) {
+                                                    var userName = "";
+                                                    angular.forEach(scope.users, function (value, key) {
+                                                        if (userFormKey == value.userId) {
+                                                            userName = value.name;
+                                                        }
+                                                    });
+                                                    scope.dynamicPopover[attrs.name][userFormFieldKey] += userName + ": " + userFormFieldValue +"\n";
+                                                    scope.$apply();
+                                                }
+                                            });
+                                        });*/
+                                        //}
+                                        //});
+                                        break;
+                                    default:
+                                        console.log("No matching resp type...");
+                                    }
+                                }
+                            }
+
+                            element.on("change keyup", function () {
+                                triggerFormChangeReq(form, formFieldData, reqJson);
                             });
                         }
                     }
